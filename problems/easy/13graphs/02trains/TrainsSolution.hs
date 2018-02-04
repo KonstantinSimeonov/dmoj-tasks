cad68addd138deeed7f803708002fa751f8d8037
Konstantin Simeonov <kon.simeonov@protonmail.com>
2018-02-04 04:08:26 +0200

    Not having a multiset in the standard library causes having pain in the life

--- problems/easy/13graphs/02trains/TrainsSolution.hs
+++ problems/easy/13graphs/02trains/TrainsSolution.hs
@@ -10,23 +10,49 @@ import qualified Data.ByteString.Lazy.Char8 as B8
 import Data.Char (ord)
 
 parseInt :: B8.ByteString -> Int32
-parseInt str = parseInt' str 0 where
-    parseInt' str !acc
-        | B8.null str  = acc
-        | otherwise = parseInt' (B8.tail str) $ fromIntegral (ord (B8.head str)) - 48 + acc * 10
-
-cykiEPedal !x !y = x + y
-
-solve :: Int -> Int -> [(Int32, Int32)] -> Int
-solve maxT = solve' Map.empty
+parseInt str = parseInt' str 0
     where
-        solve' :: Map.Map Int32 Int32 -> Int -> [(Int32, Int32)] -> Int
+        parseInt' str !acc
+            | B8.null str  = acc
+            | otherwise = parseInt' (B8.tail str) $ fromIntegral (ord (B8.head str)) - 48 + acc * 10
+
+
+data MultiSet a = MultiSet 
+    { records :: Map.Map a Int32
+    , msSize :: !Int32
+    }
+
+insert :: Ord a => a -> MultiSet a -> MultiSet a
+insert x ms = let newRecords = Map.insertWith (+) x 1 $ records ms
+              in MultiSet { records = newRecords, msSize = msSize ms + 1 }
+
+popMax :: Ord a => MultiSet a -> MultiSet a
+popMax ms = let newRecords = Map.updateMax (\v -> if v == 1 then Nothing else Just $ v - 1) $ records ms
+            in MultiSet { records = newRecords, msSize = msSize ms - 1 }
+
+right :: Ord a => a -> MultiSet a -> MultiSet a
+right b ms = MultiSet { records = newRecords, msSize = msSize ms - removedCount }
+    where
+        (newRecords, removedCount) = getBigger 0 (records ms)
+
+        getBigger !s rs
+            | Map.null rs = (rs, s)
+            | otherwise = let (first, count) = Map.findMin rs
+                          in if first > b then (rs, s) else getBigger (s + count) (Map.deleteMin rs)
+
+emptyMS :: Ord a => MultiSet a
+emptyMS = MultiSet { records = Map.empty, msSize = 0 }
+
+solve :: Int32 -> Int32 -> [(Int32, Int32)] -> Int32
+solve maxT = solve' emptyMS
+    where
+        solve' :: MultiSet Int32 -> Int32 -> [(Int32, Int32)] -> Int32
         solve' _ !ans [] = ans
         solve' bag !ans ((s, e):rest) = 
-            let updatedBag       = Map.insertWith cykiEPedal e 1 $ snd $ Map.split s bag
-                (newBag, newAns) = if Map.size updatedBag > maxT 
-                                        then (Map.deleteMax updatedBag, ans - 1)
-                                        else (updatedBag, ans)
+            let passengers       = insert e $ right s bag
+                (newBag, newAns) = if msSize passengers > maxT
+                                        then (popMax passengers, ans - 1)
+                                        else (passengers, ans)
             in solve' newBag newAns rest
 
 pairs :: [(a, a)] -> [a] -> [(a, a)]
@@ -35,7 +61,7 @@ pairs acc (x:y:xs) = pairs ((x, y):acc) xs
 
 mainByteStr :: ByteString -> ByteString
 mainByteStr input = let (n:m:l:travels) = map parseInt $! B8.words input
-                        in toLazyByteString $ intDec $ solve (fromIntegral m) (fromIntegral n) $ sort $ pairs [] travels
+                    in toLazyByteString $ intDec $ fromIntegral $ solve (fromIntegral m) (fromIntegral n) $ sort $ pairs [] travels
 
 main = B8.interact mainByteStr
 
